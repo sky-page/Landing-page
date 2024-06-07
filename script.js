66f46701422cf68d52858eb36eb02195ec952567
sky-page <windscorp333@gmail.com>
2024-06-08 03:18:49 +0700

    menambah fitur responsif navbar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function klik(x){
     x.classList.toggle("change")
-}
+};
+
+const ambil = document.querySelector('.iconMenu');
+var dd = document.querySelector('.dropdown')
+
+ambil.addEventListener('click', function(){
+    dd.classList.toggle('show');
+})
